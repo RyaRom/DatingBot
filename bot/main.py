7690c360366711630d16183f16ee2e88d30d07fc
Roman Ryabikin <riabikin.roman@gmail.com>
2025-01-26 19:10:01 +0300

    init: add main class, all dependencies

---
+++ bot/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+
+async def main():
+    load_dotenv()
+    key = os.getenv('BOT_KEY')
+    bot = Bot(token=key, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    # dp.include_router()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
